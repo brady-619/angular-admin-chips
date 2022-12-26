62bcc38b150c488ff06a94aee89e8c331ded1a45
brady-619 <daniel.montano@dish.com.mx>
2022-12-26 16:47:47 -0600

    get proveedores completo

--- src/app/proveedores/proveedores.page.ts
+++ src/app/proveedores/proveedores.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GetProveedoresService} from '../services/get-proveedores.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-proveedores',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProveedoresPage implements OnInit {
 
-  constructor() { }
+data:any;
 
-  ngOnInit() {
+  constructor(private proveedores: GetProveedoresService,private http: HttpClient) { }
+
+ async ngOnInit() {
+
+    await this.proveedores.GetProveedores().then(resp =>  {
+      
+      console.log("resp",resp)
+
+      this.data = resp.data;
+
+      }).catch(error=> {
+        /* Código a realizar cuando se rechaza la promesa */
+        console.log("NO paso chido",error)
+      });
+  
+  
+  
+  
+  
+  
+  
+  
+  
   }
+
+  
 
 }
